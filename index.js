58ac09a177df0cc015a92b7c9f7c273e69edede5
Chandler Farley <chandlerfarley@live.com>
2023-04-10 17:18:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    /* return drivers.filter(driver => 
+    driver.toUpperCase() === names.toUpperCase()
+    ); */
+    return drivers.filter(function(driver) {
+        if (driver.toUpperCase() === names.toUpperCase()) {
+            return true;
+        } else if (driver.toUpperCase() !== names.toUpperCase()) {
+            return false;
+        }
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(driver => driver.startsWith(string));
+}
+
+function matchName(array, string) {
+    return array.filter(obj => obj.name === string);
+}
